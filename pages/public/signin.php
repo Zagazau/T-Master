036d15c21858c5fe981ca28863a22fae84ac0ab3
tiagop23 <talexandrea@ipvc.pt>
2023-12-20 15:21:20 +0000

    login page

--- pages/public/signin.php
+++ pages/public/signin.php
@@ -20,26 +20,50 @@ $title = ' - Sign In';
       unset($_SESSION['errors']);
     }
     ?>
-  </section>
-  <form action="/crud/controllers/auth/signin.php" method="post">
-    <h1 class="h3 mb-3 fw-normal">Sign In</h1>
-    <div class="form-floating mb-2">
-      <input type="email" class="form-control" id="Email" placeholder="Email" name="email" maxlength="255"
-        value="<?= isset($_REQUEST['email']) ? $_REQUEST['email'] : null ?>">
-      <label for="Email">Email</label>
-    </div>
-    <div class="form-floating mb-2">
-      <input type="password" class="form-control" id="password" placeholder="Password" name="password" maxlength="255"
-        value="<?= isset($_REQUEST['password']) ? $_REQUEST['password'] : null ?>">
-      <label for="password">Password</label>
-    </div>
-    <div class="checkbox mb-3">
-      <label><input type="checkbox" value="remember-me">Remember me</label>
-    </div>
-    <button class="w-100 btn btn-lg btn-success mb-2" type="submit" name="user" value="login">Sign In</button>
-  </form>
-  <a href="/crud"><button class="w-100 btn btn-lg btn-info">Back</button></a>
-</main>
-<?php
-include_once __DIR__ . '../../../templates/footer.php';
-?>
+
+    <section class="vh-100">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-sm-6 text-black">
+
+            <div class="px-5 ms-xl-4">
+              <img class="header-logo" src="./assets/images/Logo1.png" alt="">
+            </div>
+
+            <img class="header-logo" src="./assets/images/Logo1.png" alt="">
+
+            <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
+
+              <form style="width: 23rem;">
+
+                <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Log in</h3>
+
+                <div class="form-outline mb-4">
+                  <input type="email" id="form2Example18" class="form-control form-control-lg" placeholder="Email" />
+                </div>
+
+                <div class="form-outline mb-4">
+                  <input type="password" id="form2Example28" class="form-control form-control-lg"
+                    placeholder="password" />
+                </div>
+
+                <div class="pt-1 mb-4">
+                  <button class="btn btn-info btn-lg btn-block" type="button">Login</button>
+                </div>
+
+                <p class="small mb-5 pb-lg-2"><a class="text-muted" href="#!">Forgot password?</a></p>
+                <p>Não tens conta? <a href="./pages/public/signup.php" class="link-info">Regista-te aqui</a></p>
+
+              </form>
+
+            </div>
+
+          </div>
+          <div class="col-sm-6 px-0 d-none d-sm-block">
+            <img
+              src="https://img.freepik.com/premium-photo/vertical-background-image-minimal-home-office-workplace-with-laptop-accessories-black-white-copy-space_236854-27485.jpg?w=360"
+              alt="Login image" class="w-100 vh-100" style="width: 200px; height: auto;">
+          </div>
+        </div>
+      </div>
+    </section>
